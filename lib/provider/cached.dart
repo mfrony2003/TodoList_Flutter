9bf4a3c7367b9d0c1d6177d1feaeedadb1696776
mfrony2003 <mfrony2003@gmail.com>
2021-07-01 01:49:31 +0600

    dark Mode

---
+++ lib/provider/cached.dart
@@ -0,0 +1,8 @@
+class CachedData {
+  final String dialogTitle = "Add a task to your list";
+  final String hint = "Enter task here";
+  final String add = "ADD";
+  final String update = "UPDATE";
+  final String delete = "DELETE";
+  final String cancel = "Cancel";
+}
